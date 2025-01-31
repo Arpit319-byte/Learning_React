f667b2bbd26b4611b8bd0e0b5245fcc671310b56
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2025-01-31 22:39:44 +0530

    Added few changes to the files.

---
+++ Lesson1/App.js
@@ -0,0 +1,8 @@
+const heading=React.createElement("h1",{
+    id:"heading"
+},"Hello Starting the JavaScript");
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+console.log(heading);
+console.log(root);
